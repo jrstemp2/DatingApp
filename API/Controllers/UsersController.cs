186a91f60de3d8b47505ee1d0d1d00f2ed1fce2c
John Stempowski <jrstemp2@gmail.com>
2023-08-12 10:19:22 -0400

    initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    // inject the db context and set it up
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync() 
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser?>> GetUserAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
---
+++ API/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext: DbContext
+{
+    // Adding a constructor
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<AppUser> Users { get; set; } // Users will be the name of the DB Table
+}
